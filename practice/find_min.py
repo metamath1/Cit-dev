049745e151ed3484e68ea1375d4bf774523501e5
Joonu Jo <metamath@gmail.com>
2023-04-05 23:25:15 +0900

    modify min function name

--- practice/find_min.py
+++ practice/find_min.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-def min(L: list) -> Union[int,float]: 
+def my_min(L: list) -> Union[int,float]: 
     v = L[0]
 
     for i in L[1:]:
@@ -9,4 +9,5 @@ def min(L: list) -> Union[int,float]:
     
     return v
 
+print( my_min([4.4, 156, 11, 45, 31, 98]) )
 print( min([4.4, 156, 11, 45, 31, 98]) )
